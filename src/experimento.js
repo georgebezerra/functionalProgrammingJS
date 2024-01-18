505e76ede9aa0f8e963fc0241e69ff203e52ead9
George Bezerra <hi@georgebezerra.dev>
2024-01-18 08:34:05 -0300

    Initial experimento

--- src/experimento.js
+++ src/experimento.js
@@ -1,5 +1,21 @@
-function fn() {
+function fn5() {
+  console.log('timeout')
+}
 
+function fn4() {
+  fn5()
+}
+function fn3() {
+  setTimeout(() => fn4(), 3000)
+  console.log('fn3')
+}
+
+function fn2() {
+  fn3()
+}
+
+function fn1() {
+  fn2()
 }
 
 function main() {
